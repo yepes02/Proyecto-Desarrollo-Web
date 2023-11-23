0abb7c799b8fe78bbd6f7f0eda05fd6b8963a1a2
Marco Yepes <yesidyepes@gmail.com>
2023-11-22 22:11:41 -0500

    conexion modificado2

--- conexionDB.php
+++ conexionDB.php
@@ -1,19 +1,26 @@
 <?php
 
-$db_host="localhost";
-$db_nombre="usuario";
-$db_usuario="root";
-$db_contra="";
+$db_host= "localhosttt";
+$db_nombre= "usuario";
+$db_usuario= "root";
+$db_contra= "";
+
 
 $conexion = mysqli_connect($db_host, $db_usuario, $db_contra, $db_nombre);
 
-$consulta="SELECT * FROM datospersonales";
+if(mysqli_connect_errno()) { 
+    echo "LA CONEXION DE LA BASE DE DATOS HA FALLADO";
+    exit();
+}
+
+
+/*$consulta="SELECT * FROM datospersonales";
 $resultado=mysqli_query($conexion,$consulta);
 
 $fila=mysqli_fetch_row($resultado);
 echo $fila[0]. " ";
 echo $fila[1]. " ";
 echo $fila[2]. " ";
-echo $fila[3]. " ";
+echo $fila[3]. " ";*/
 
 ?>
